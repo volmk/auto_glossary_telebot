b9583fb04d5cd65d95a20320c3b68fe8ae72aa32
volmk <lvi.work1@gmail.com>
2020-03-01 22:02:55 +0200

    asd

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 
 from flask import Flask, request
+from fake_useragent import UserAgent
 
 import telebot
-from glossary import write_glossary, get_form_file
+from glossary import get_span, get_form_file
 
 TOKEN = '880907439:AAFnvUJtDKAKmQRAaEkYeUrMVkIkrUHj7ws'
 bot = telebot.TeleBot(TOKEN)
@@ -15,13 +16,23 @@ def start(message):
     bot.send_message(message.chat.id, f'Отправь сообщение вида "<code>{get_form_file()}</code>"', parse_mode='html')
 
 
-
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     cid = message.chat.id
     bot.send_chat_action(cid, 'typing')
-    glossary = write_glossary(message.str)
-    bot.send_message(cid, glossary)
+    ua = UserAgent()
+
+    words_str = message.text
+    res = ""
+    for idx, s in enumerate(words_str.split(', ')):
+        gloss = get_span(s.translate({' ': '%20'}), ua.ie)
+        line = f'{idx + 1}. {s} – {gloss}\n'
+        res += line
+        bot.send_chat_action(cid, 'typing')
+        if idx % 5 == 0:
+            bot.send_message(cid, 'Обработано слов: {idx+1}')
+
+    bot.send_message(cid, res)
 
 
 @app.route('/' + TOKEN, methods=['POST'])
