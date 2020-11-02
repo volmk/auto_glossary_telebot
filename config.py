da005b15d16462e9443f5ba2be5d4025c5ae835d
volmk <lvi.work1@gmail.com>
2020-11-02 13:27:20 +0200

    github update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEVELOP = False
-WEBHOOK_URL = 'https://tranquil-bayou-74967.herokuapp.com'
-TELEGRAM_TOKEN = '1301995094:AAHzkaJ9-iaTyj2Zgx_lUmHDpXXaVb2vyWY'  # @auto_glossary_bot
+WEBHOOK_URL = ''
+TELEGRAM_TOKEN = ''  # @auto_glossary_bot
 
 try:
     from local import *
